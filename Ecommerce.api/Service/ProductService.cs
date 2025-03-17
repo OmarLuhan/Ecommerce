f99ebeb17c42e4e57a9d2efb6aa888f7e5fd2d26
luhan <n00209455@upn.pe>
2025-03-17 15:45:25 -0500

    feat: add product service

--- Ecommerce.api/Service/ProductService.cs
+++ Ecommerce.api/Service/ProductService.cs
@@ -2,12 +2,13 @@ using AutoMapper;
 using Ecommerce.api.Dto;
 using Ecommerce.api.Model;
 using Ecommerce.api.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.api.Service;
 public interface IProductService
 {
     Task<List<ProductDto>> ListAsync(string search);
-    Task<List<ProductDto>> ListByCategoryAsync(int categoryId, string search);
+    Task<List<ProductDto>> CatalogAsync(string category, string search);
     Task<ProductDto> GetProductAsync(int id);
     Task<ProductDto> CreateAsync(ProductDto model);
     Task<bool> UpdateAsync(ProductDto model);
@@ -15,33 +16,65 @@ public interface IProductService
 }
 public class ProductService(IGenericRepository<Product> productRepository,IMapper mapper):IProductService
 {
-    public Task<List<ProductDto>> ListAsync(string search)
+    public async Task<List<ProductDto>> ListAsync(string search)
     {
-        throw new NotImplementedException();
+        IQueryable<Product>query= productRepository.Query(p=>p.Name.Contains(search));
+        return mapper.Map<List<ProductDto>>(await query.ToListAsync());
     }
 
-    public Task<List<ProductDto>> ListByCategoryAsync(int categoryId, string search)
+    public async Task<List<ProductDto>> CatalogAsync(string category, string search)
     {
-        throw new NotImplementedException();
+        IQueryable<Product> query= productRepository.Query(p=>p.Name.Contains(search)&&
+                                                              p.Category.Name.Contains(category));
+        return mapper.Map<List<ProductDto>>(await query.ToListAsync());
     }
 
-    public Task<ProductDto> GetProductAsync(int id)
+    public async Task<ProductDto> GetProductAsync(int id)
     {
-        throw new NotImplementedException();
+        IQueryable<Product>query = productRepository.Query(p=>p.Id==id);
+        Product? product = await query.FirstOrDefaultAsync();
+        if(product==null)
+            throw new TaskCanceledException("Product not found");
+        return mapper.Map<ProductDto>(product);
     }
 
-    public Task<ProductDto> CreateAsync(ProductDto model)
+    public async Task<ProductDto> CreateAsync(ProductDto model)
     {
-        throw new NotImplementedException();
+        Product product= mapper.Map<Product>(model);
+        Product newProduct = await productRepository.CreateAsync(product);
+        if(newProduct.Id == 0)
+            throw new TaskCanceledException("Failed to create product");
+        return mapper.Map<ProductDto>(newProduct);
     }
 
-    public Task<bool> UpdateAsync(ProductDto model)
+    public async Task<bool> UpdateAsync(ProductDto model)
     {
-        throw new NotImplementedException();
+        IQueryable<Product>query= productRepository.Query(p=>p.Id==model.Id);
+        Product? product = await query.FirstOrDefaultAsync();
+        if(product==null)
+            throw new TaskCanceledException("Product not found");
+        product.Name = model.Name;
+        product.Price = model.Price;
+        product.CategoryId = model.CategoryId;
+        product.Description = model.Description;
+        product.Image = model.Image;
+        product.OfferPrice = model.OfferPrice;
+        product.Stock = model.Stock;
+        bool updated = await productRepository.UpdateAsync(product);
+        if(!updated)
+            throw new TaskCanceledException("Failed to update product");
+        return updated;
     }
 
-    public Task<bool> DeleteAsync(int id)
+    public async Task<bool> DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        IQueryable<Product>query= productRepository.Query(p=>p.Id==id);
+        Product? product = await query.FirstOrDefaultAsync();
+        if(product==null)
+            throw new TaskCanceledException("Product not found");
+        bool deleted = await productRepository.DeleteAsync(product);
+        if(!deleted)
+            throw new TaskCanceledException("Failed to delete product");
+        return deleted;
     }
 }
