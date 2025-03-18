7488f8e76b5a632c40ed623be60922075ae45ab1
luhan <n00209455@upn.pe>
2025-03-18 16:32:12 -0500

    feat : add controller category, an methods , get, post, put , deete

--- Ecommerce.api/Controllers/CategoryController.cs
+++ Ecommerce.api/Controllers/CategoryController.cs
@@ -7,41 +7,20 @@ using Microsoft.AspNetCore.Mvc;
 namespace Ecommerce.api.Controllers;
 
 [Route("api/[controller]")]
-[ApiController]
-public class UserController(IUserService service) : ControllerBase
+public class CategoryController(ICategoryService service) : ControllerBase
 {
-    [HttpGet("List/{role:alpha}/{search:alpha?}")]
+    [HttpGet("list/{search:alpha?}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<Response<List<UserDto>>>> List(string role, string search = "NA")
+    public async Task<ActionResult<Response<List<CategoryDto>>>> ListAsync(string search = "NA")
     {
-        var response = new Response<List<UserDto>>();
+        var response = new Response<List<CategoryDto>>();
+        if (search == "NA") search = "";
         try
         {
-            if (search == "NA") search = "";
-            response.Status= HttpStatusCode.OK;
+            response.Status = HttpStatusCode.OK;
             response.Success = true;
-            response.Data = await service.ListAsync(role, search);
-            return Ok(response);
-        }
-        catch (Exception ex)
-        {
-            response.Message = ex.Message;
-            response.Success = false;
-            return StatusCode(500, response);
-        }
-    }
-    [HttpGet("Get/{id:int}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<Response<UserDto>>> Get(int id)
-    {
-        var response = new Response<UserDto>();
-        try
-        {
-            response.Status= HttpStatusCode.OK;
-            response.Success = true;
-            response.Data = await service.GetUserAsync(id);
+            response.Data = await service.ListAsync(search);
             return Ok(response);
         }
         catch (Exception ex)
@@ -52,63 +31,82 @@ public class UserController(IUserService service) : ControllerBase
             return StatusCode(500, response);
         }
     }
-    [HttpPost("Add")]
-    [ProducesResponseType(StatusCodes.Status201Created)]
+    [HttpGet("Get/{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<Response<UserDto>>> Add([FromBody]UserDto user)
+    public async Task<ActionResult<Response<CategoryDto>>> Get(int id)
     {
-        var response = new Response<UserDto>();
+        var response = new Response<CategoryDto>();
         try
         {
-            response.Status= HttpStatusCode.Created;
+            response.Status = HttpStatusCode.OK;
             response.Success = true;
-            response.Data = await service.CreateAsync(user);
+            response.Data = await service.GetCategoryAsync(id);
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            response.Status = HttpStatusCode.InternalServerError;
+            response.Message = ex.Message;
+            response.Success = false;
+            return StatusCode(500, response);
+        }
+    }
+    [HttpPost("add")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<Response<CategoryDto>>> AddAsync([FromBody] CategoryDto category)
+    {
+        var response = new Response<CategoryDto>();
+        try
+        {
+            response.Status = HttpStatusCode.Created;
+            response.Success = true;
+            response.Data = await service.CreateAsync(category);
             return CreatedAtAction(nameof(Get), new { id = response.Data.Id }, response);
         }
         catch (Exception ex)
         {
-            response.Status= HttpStatusCode.InternalServerError;
+            response.Status = HttpStatusCode.InternalServerError;
             response.Message = ex.Message;
             response.Success = false;
             return StatusCode(500, response);
         }
     }
-
-    [HttpPost("Authorize")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<Response<SessionDto>>> Authorize([FromBody] LoginDto login)
-    {
-        var response = new Response<SessionDto>();
-        try
-        {
-            response.Status= HttpStatusCode.OK;
-            response.Success = true;
-            response.Data = await service.AuthorizeAsync(login);
-            return Ok(response);
-
-        }
-        catch (Exception ex)
-        {
-            response.Status= HttpStatusCode.InternalServerError;
-            response.Message = ex.Message;
-            response.Success = false;
-            return StatusCode(500, response);
-        }
-    }
-    [HttpPut("Update")]
+    [HttpPut("update")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> Update([FromBody] UserDto user)
+    public async Task<IActionResult> UpdateAsync([FromBody] CategoryDto category)
     {
         try
         {
-            _ =await service.UpdateAsync(user);
+            _ = await service.UpdateAsync(category);
             return NoContent();
         }
         catch (Exception ex)
         {
-            var response = new Response<UserDto>
+            var response = new Response<CategoryDto>()
+            {
+                Status = HttpStatusCode.InternalServerError,
+                Message = ex.Message,
+                Success = false
+            };
+            return StatusCode(500, response);
+        }
+    }
+    [HttpDelete("delete/{id:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        try
+        {
+            _ = await service.DeleteAsync(id);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            var response = new Response<CategoryDto>()
             {
                 Status = HttpStatusCode.InternalServerError,
                 Message = ex.Message,
@@ -118,25 +116,4 @@ public class UserController(IUserService service) : ControllerBase
         }
     }
     
-    [HttpDelete("Delete/{id:int}")]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> Delete(int id)
-    {
-        try
-        {
-            _ =await service.DeleteAsync(id);
-            return NoContent();
-        }
-        catch (Exception ex)
-        {
-            var response = new Response<UserDto>
-            {
-                Status = HttpStatusCode.InternalServerError,
-                Message = ex.Message,
-                Success = false
-            };
-            return StatusCode(500, response);
-        }
-    }
 }
