6027e30954a201fb1ce478e9fc34972fdf84ebb3
luhan <n00209455@upn.pe>
2025-03-21 15:37:47 -0500

    add category

--- Ecommerce.webAssembly/Dto/CategoryDto.cs
+++ Ecommerce.webAssembly/Dto/CategoryDto.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Ecommerce.webAssembly.Dto;
 
 public class CategoryDto
 {
     public int Id { get; set; }
-
+[Required]
     public string Name { get; set; } = null!;
 }
